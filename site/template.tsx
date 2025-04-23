bfefa85f31c22aac0d674eadfdb2b218590d2fee
sdegutis <sbdegutis@gmail.com>
2025-04-22 19:34:47 -0500

    Better font.

--- site/template.tsx
+++ site/template.tsx
@@ -10,7 +10,7 @@ export function mainPage(current: string, posts: { path: string, title: string }
         <title>90s.dev</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@200..800&family=Geo:ital@0;1&family=Michroma&family=Oxanium:wght@200..800&family=Silkscreen:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@200..800&family=Oxanium:wght@200..800&family=Silkscreen:wght@400;700&display=swap" rel="stylesheet" />
         <script type="module" src="/script/runcode.js"></script>
       </head>
       <body>
