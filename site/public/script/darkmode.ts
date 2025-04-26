3a63c482546c61ad84ff019a515a765cf9f202bd
sdegutis <sbdegutis@gmail.com>
2025-04-26 11:40:28 -0500

    Better.

--- site/public/script/darkmode.ts
+++ site/public/script/darkmode.ts
@@ -1,16 +1,26 @@
 (() => {
-  const set = (mode: string) => {
-    document.documentElement.classList.remove('darkmode', 'lightmode')
-    if (mode) document.documentElement.classList.add(mode)
-    localStorage.setItem('darkmode', mode)
-  }
-
-  const lastmode = localStorage.getItem('darkmode')
-  if (lastmode !== null) set(lastmode)
 
   const toggledarkmode = document.querySelector('#toggledarkmode') as HTMLDivElement
   const [dark, light, system] = toggledarkmode.querySelectorAll('a')
-  dark.onclick = (e) => { e.preventDefault(); set('darkmode') }
-  light.onclick = (e) => { e.preventDefault(); set('lightmode') }
-  system.onclick = (e) => { e.preventDefault(); set('') }
+
+  const buttons = { dark, light, '': system }
+  const modes = new Map(Object.entries(buttons).map(([k, v]) => [v, k]))
+
+  dark.onclick = light.onclick = system.onclick = (e) => {
+    e.preventDefault()
+    set(modes.get(e.target as HTMLAnchorElement)!)
+  }
+
+  const set = (mode: string) => {
+    document.documentElement.classList.remove('dark', 'light')
+    if (mode) document.documentElement.classList.add(mode)
+    localStorage.setItem('dark', mode)
+    for (const [m, button] of Object.entries(buttons)) {
+      button.classList.toggle('current', mode === m)
+    }
+  }
+
+  const lastmode = localStorage.getItem('dark')
+  set(lastmode ?? '')
+
 })()
