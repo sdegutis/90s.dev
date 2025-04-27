0ef9fc346fb266ea8ac44b6bb85fdb7732426199
sdegutis <sbdegutis@gmail.com>
2025-04-26 21:31:45 -0500

    TOC script.

--- site/public/script/runcode.ts
+++ site/public/script/runcode.ts
@@ -2,68 +2,6 @@ import monaco from './monaco.js'
 import { makeMonacoFancier } from './token-provider.js'
 
 
-function trackNavSection() {
-
-  const navlinks = document.querySelectorAll<HTMLAnchorElement>('#toc a')
-  const root = document.querySelector('main')!
-  const visible = new Set<HTMLElement>()
-
-  function firstHeaderBefore(el: Element) {
-    let node: Element | null = el
-    while (node) {
-      if (node.tagName.match(/^H\d$/)) return node
-      node = node.previousElementSibling
-    }
-    return node
-  }
-
-  function highestStartingAt() {
-    const ordered = [...visible].sort((a, b) => {
-      if (a.offsetTop < b.offsetTop) return -1
-      if (a.offsetTop > b.offsetTop) return +1
-      return 0
-    })
-    return ordered.find(el => el.tagName.match(/^H\d$/))
-      ?? firstHeaderBefore(ordered[0])
-  }
-
-  const observer = new IntersectionObserver(records => {
-    for (const r of records) {
-      if (r.isIntersecting)
-        visible.add(r.target as HTMLElement)
-      else
-        visible.delete(r.target as HTMLElement)
-    }
-
-    const header = highestStartingAt()
-    if (!header) return
-
-    for (const a of navlinks) {
-      a.classList.toggle('current', a.hash.slice(1) === header.id)
-    }
-  }, { threshold: 1 })
-
-  for (const node of [...root.children].slice(1, -1)) {
-    observer.observe(node)
-  }
-
-}
-
-trackNavSection()
-
-
-
-checkhash()
-window.addEventListener('hashchange', checkhash)
-
-function checkhash() {
-  for (const a of document.querySelectorAll('a')) {
-    if (a.hash) {
-      a.classList.toggle('current', a.hash === location.hash)
-    }
-  }
-}
-
 for (const button of document.querySelectorAll<HTMLElement>('#mobileheader>span')) {
   const first = button.nextElementSibling
   button.onclick = () => {
---
+++ site/public/script/toc.ts
@@ -0,0 +1,53 @@
+const navlinks = document.querySelectorAll<HTMLAnchorElement>('#toc a')
+const root = document.querySelector('main')!
+const visible = new Set<HTMLElement>()
+
+function firstHeaderBefore(el: Element) {
+  let node: Element | null = el
+  while (node) {
+    if (node.tagName.match(/^H\d$/)) return node
+    node = node.previousElementSibling
+  }
+  return node
+}
+
+function highestStartingAt() {
+  const ordered = [...visible].sort((a, b) => {
+    if (a.offsetTop < b.offsetTop) return -1
+    if (a.offsetTop > b.offsetTop) return +1
+    return 0
+  })
+  return ordered.find(el => el.tagName.match(/^H\d$/))
+    ?? firstHeaderBefore(ordered[0])
+}
+
+const observer = new IntersectionObserver(records => {
+  for (const r of records) {
+    if (r.isIntersecting)
+      visible.add(r.target as HTMLElement)
+    else
+      visible.delete(r.target as HTMLElement)
+  }
+
+  const header = highestStartingAt()
+  if (!header) return
+
+  for (const a of navlinks) {
+    a.classList.toggle('current', a.hash.slice(1) === header.id)
+  }
+}, { threshold: 1 })
+
+for (const node of [...root.children].slice(1, -1)) {
+  observer.observe(node)
+}
+
+checkhash()
+window.addEventListener('hashchange', checkhash)
+
+function checkhash() {
+  for (const a of document.querySelectorAll('a')) {
+    if (a.hash) {
+      a.classList.toggle('current', a.hash === location.hash)
+    }
+  }
+}
