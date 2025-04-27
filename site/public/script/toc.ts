8c698a2fecf8ac152cebdb58d967aa8b1c4c0ddf
sdegutis <sbdegutis@gmail.com>
2025-04-27 09:52:04 -0500

    Cleaner but still wrong.

--- site/public/script/toc.ts
+++ site/public/script/toc.ts
@@ -1,28 +1,9 @@
 const navlinks = document.querySelectorAll<HTMLAnchorElement>('#toc a')
-const root = document.querySelector('main')!
 const visible = new Set<HTMLElement>()
-
-function firstHeaderBefore(el: Element) {
-  let node: Element | null = el
-  while (node) {
-    if (node.tagName.match(/^H\d$/)) return node
-    node = node.previousElementSibling
-  }
-  return node
-}
-
-function highestStartingAt() {
-  const ordered = [...visible].sort((a, b) => {
-    if (a.offsetTop < b.offsetTop) return -1
-    if (a.offsetTop > b.offsetTop) return +1
-    return 0
-  })
-  return ordered.find(el => el.tagName.match(/^H\d$/))
-    ?? firstHeaderBefore(ordered[0])
-}
 
 const observer = new IntersectionObserver(records => {
   records.sort((a, b) => a.time < b.time ? -1 : a.time > b.time ? 1 : 0)
+
   for (const r of records) {
     if (r.isIntersecting)
       visible.add(r.target as HTMLElement)
@@ -30,25 +11,39 @@ const observer = new IntersectionObserver(records => {
       visible.delete(r.target as HTMLElement)
   }
 
-  const header = highestStartingAt()
-  if (!header) return
+  const refdHeaders = [...visible].map(el => headerMap.get(el)!).sort((a, b) => {
+    const ai = allHeaders.indexOf(a)
+    const bi = allHeaders.indexOf(b)
+    if (ai < bi) return -1
+    if (ai > bi) return +1
+    return 0
+  })
+
+  const header = refdHeaders[0]
 
   for (const a of navlinks) {
     a.classList.toggle('current', a.hash.slice(1) === header.id)
   }
 }, { threshold: 1 })
 
-for (const node of [...root.children].slice(1, -1)) {
+const headerMap = new Map<HTMLElement, HTMLElement>()
+const tops = [...document.querySelectorAll<HTMLElement>('main>*, main>section>*')].slice(1, -1)
+const allHeaders: HTMLElement[] = []
+for (const node of tops) {
+  if (node.tagName.match(/^H\d$/)) {
+    allHeaders.push(node)
+  }
+  headerMap.set(node, allHeaders.at(-1)!)
   observer.observe(node)
 }
 
-checkhash()
-window.addEventListener('hashchange', checkhash)
+// checkhash()
+// window.addEventListener('hashchange', checkhash)
 
-function checkhash() {
-  for (const a of document.querySelectorAll('a')) {
-    if (a.hash) {
-      a.classList.toggle('current', a.hash === location.hash)
-    }
-  }
-}
+// function checkhash() {
+//   for (const a of document.querySelectorAll('a')) {
+//     if (a.hash) {
+//       a.classList.toggle('current', a.hash === location.hash)
+//     }
+//   }
+// }
