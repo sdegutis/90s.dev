ccb526047d0d9dc4eaa0f50b05c2124245015e9d
sdegutis <sbdegutis@gmail.com>
2025-04-27 09:59:03 -0500

    Finally!

--- site/public/script/toc.ts
+++ site/public/script/toc.ts
@@ -1,7 +1,10 @@
+const tops = [...document.querySelectorAll<HTMLElement>('main>*, main>section>*')].slice(1, -1)
 const navlinks = document.querySelectorAll<HTMLAnchorElement>('#toc a')
+const headers = new Map<HTMLElement, HTMLElement>()
 const visible = new Set<HTMLElement>()
 
 const observer = new IntersectionObserver(records => {
+
   records.sort((a, b) => a.time < b.time ? -1 : a.time > b.time ? 1 : 0)
 
   for (const r of records) {
@@ -11,30 +14,32 @@ const observer = new IntersectionObserver(records => {
       visible.delete(r.target as HTMLElement)
   }
 
-  const refdHeaders = [...visible].map(el => headerMap.get(el)!).sort((a, b) => {
-    const ai = allHeaders.indexOf(a)
-    const bi = allHeaders.indexOf(b)
-    if (ai < bi) return -1
-    if (ai > bi) return +1
-    return 0
-  })
-
-  const header = refdHeaders[0]
+  const seen = [...visible].sort(sortTops)
+  const header =
+    seen.find(el => el.tagName.match(/^H\d$/))
+    ?? headers.get(seen[0])!
 
   for (const a of navlinks) {
     a.classList.toggle('current', a.hash.slice(1) === header.id)
   }
-}, { threshold: 1 })
 
-const headerMap = new Map<HTMLElement, HTMLElement>()
-const tops = [...document.querySelectorAll<HTMLElement>('main>*, main>section>*')].slice(1, -1)
+}, { threshold: .5 })
+
 const allHeaders: HTMLElement[] = []
 for (const node of tops) {
   if (node.tagName.match(/^H\d$/)) {
     allHeaders.push(node)
   }
-  headerMap.set(node, allHeaders.at(-1)!)
+  headers.set(node, allHeaders.at(-1)!)
   observer.observe(node)
+}
+
+function sortTops(a: HTMLElement, b: HTMLElement) {
+  const ai = tops.indexOf(a)
+  const bi = tops.indexOf(b)
+  if (ai < bi) return -1
+  if (ai > bi) return +1
+  return 0
 }
 
 // checkhash()
