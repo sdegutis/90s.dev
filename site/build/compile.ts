a3b01752c44b4af2bc3c811f9a2b1bf8da51c2f5
sdegutis <sbdegutis@gmail.com>
2025-04-26 21:12:43 -0500

    Fix.

--- site/build/compile.ts
+++ site/build/compile.ts
@@ -3,9 +3,10 @@ import ts from 'typescript'
 export function compileTsx(str: string) {
   return ts.transpileModule(str, {
     compilerOptions: {
-      inlineSourceMap: true,
-      jsx: ts.JsxEmit.ReactJSX,
+      target: ts.ScriptTarget.ESNext,
       module: ts.ModuleKind.ESNext,
+      jsx: ts.JsxEmit.ReactJSX,
+      inlineSourceMap: true,
     }
   }).outputText
 }
