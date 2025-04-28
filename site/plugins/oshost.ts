b00ab5f951f42a245b96d9f86ce4b74f2648479b
sdegutis <sbdegutis@gmail.com>
2025-04-27 23:28:06 -0500

    Fine.

--- site/plugins/oshost.ts
+++ site/plugins/oshost.ts
@@ -2,12 +2,13 @@ import type MarkdownIt from "markdown-it"
 import { oshost } from "../../globals.ts"
 import { defaultRender, type Env } from "./markdown.ts"
 
+const escaped = encodeURI('${OSHOST}')
+
 export function checkForOsHost(md: MarkdownIt) {
   const linkopen = md.renderer.rules["link_open"] ?? defaultRender
   md.renderer.rules["link_open"] = (tokens, idx, opts, env: Env, self) => {
     const tok = tokens[idx]
     const href = tok.attrGet('href')!
-    const escaped = encodeURI('${OSHOST}')
 
     if (href.startsWith(escaped)) {
       tok.attrSet('href', href.replace(escaped, oshost))
