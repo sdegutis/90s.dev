8c76a69c1d55d17a3e5f048bbec5a9f210582a1d
sdegutis <sbdegutis@gmail.com>
2025-04-27 21:06:21 -0500

    dts

--- jsx.d.ts
+++ jsx.d.ts
@@ -1,14 +1,6 @@
 declare namespace JSX {
-
   type IntrinsicElements = Record<string, any>
-
   interface ElementChildrenAttribute {
     children: any
   }
-
-}
-
-declare module "*.md" {
-  const text: string
-  export default text
 }
