b5a6d81da07d0333107fee2c8af146e2c84f2868
sdegutis <sbdegutis@gmail.com>
2025-05-09 01:33:32 -0500

    Fix.

--- site/public/script/links.ts
+++ site/public/script/links.ts
@@ -1,26 +1,24 @@
 for (const a of document.querySelectorAll('a')) {
-  if (!a.href.startsWith(location.origin)) {
-    if (a.href.startsWith('/os/#')) {
-      a.onclick = (e) => {
-        if (e.ctrlKey) return
-        e.preventDefault()
+  if (a.href.startsWith(location.origin + '/os/#')) {
+    a.onclick = (e) => {
+      if (e.ctrlKey) return
+      e.preventDefault()
 
-        const maybe = a.parentElement?.nextElementSibling
-        if (maybe?.tagName === 'IFRAME') {
-          maybe.remove()
-          return
-        }
-
-        const iframe = document.createElement('iframe')
-        iframe.className = 'embedded'
-        iframe.width = '640'
-        iframe.height = '360'
-        iframe.src = a.href
-        a.parentElement?.insertAdjacentElement('afterend', iframe)
+      const maybe = a.parentElement?.nextElementSibling
+      if (maybe?.tagName === 'IFRAME') {
+        maybe.remove()
+        return
       }
+
+      const iframe = document.createElement('iframe')
+      iframe.className = 'embedded'
+      iframe.width = '640'
+      iframe.height = '360'
+      iframe.src = a.href
+      a.parentElement?.insertAdjacentElement('afterend', iframe)
     }
-    else {
-      a.target = '_blank'
-    }
+  }
+  else if (!a.href.startsWith(location.origin) || a.href === location.origin + '/os/') {
+    a.target = '_blank'
   }
 }
