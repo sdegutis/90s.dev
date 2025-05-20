dd19b4e5a54b01ebeb0461d78f37b94be4de9d9e
sdegutis <sbdegutis@gmail.com>
2025-05-20 09:46:15 -0500

    Fine?

--- site/template/core.tsx
+++ site/template/core.tsx
@@ -61,13 +61,17 @@ export function Navbar(data: {
     'technical': 'Technical',
     'reference': 'Reference',
     'community': 'Community',
+    'blog': 'Blog',
   })
 
   const pages = [
     ...data.pages,
-    { section: 'community', title: 'GH Issues', path: 'https://www.reddit.com/r/90s_dev/' },
-    { section: 'community', title: 'GH Wiki', path: 'https://www.reddit.com/r/90s_dev/wiki/index/' },
-    { section: 'community', title: 'GH Discussions', path: 'https://www.reddit.com/r/90s_dev/' },
+    { section: 'community', title: 'GH Issues', path: 'https://github.com/sdegutis/90s.dev/issues' },
+    { section: 'community', title: 'GH Wiki', path: 'https://github.com/sdegutis/90s.dev/wiki' },
+    { section: 'community', title: 'GH Discussions', path: 'https://github.com/sdegutis/90s.dev/discussions' },
+    // { section: 'community', title: 'GH Issues', path: 'https://www.reddit.com/r/90s_dev/' },
+    // { section: 'community', title: 'GH Wiki', path: 'https://www.reddit.com/r/90s_dev/wiki/index/' },
+    // { section: 'community', title: 'GH Discussions', path: 'https://www.reddit.com/r/90s_dev/' },
   ]
 
   const groups = Map.groupBy(pages, p => p.section)
